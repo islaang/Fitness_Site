1577806cb64ae412880fd73cbac17557f17ae33a
islam <ikuabdykaarov@gmail.com>
2024-11-11 13:22:26 +0600

    Added .gitignore

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'core'
+    'core',
+    'templates',
+    'workout_site'
+
 ]
 
 MIDDLEWARE = [
